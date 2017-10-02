87b20c948ec345aca817bc73fd21f00dedc1b5a6
Nikita <iamlilacpenguin@gmail.com>
2017-10-02 13:07:31 +0300

    manual arg parser

--- lpargs/cli.py
+++ lpargs/cli.py
@@ -1,22 +1,32 @@
 from __future__ import absolute_import
 
 import argparse
+from argparse import Namespace
 import sys
+from collections import deque
+
 from lpargs.parser import LArgsParser
 from shlex import quote
 
 
+def parse_args():
+    raw_args = deque(sys.argv)
+    args = Namespace(brackets='{}', delimiter=',', template=[])
+    raw_args.popleft()
+    while raw_args:
+        arg = raw_args.popleft()
+        if arg in {'-d', '--delimiter'}:
+            args.delimiter = raw_args.popleft()
+        elif arg in {'-b', '--brackets'}:
+            args.brackets = raw_args.popleft()
+        else:
+            args.template = [arg] + list(raw_args)
+            break
+    return args
+
+
 def main():
-    p = argparse.ArgumentParser(description="Possible operators:\n"
-                                            "* {index}: take row with index = {index}\n"
-                                            "* e: strip extension\n"
-                                            "* E: take extension\n"
-                                            "* b: basename\n"
-                                            "* d: dirname")
-    p.add_argument('--delimiter', '-d', default='\t')
-    p.add_argument('--brackets', '-b', default='{}')
-    p.add_argument('template', nargs=argparse.REMAINDER)
-    args = p.parse_args()
+    args = parse_args()
 
     template = ' '.join(
         quote(arg).replace('\-', '-')
